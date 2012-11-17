a05ee4da6bff1705ed44498d46e13a81947f7d42
ufownl@gmail.com <ufownl@gmail.com@d2a28e59-fe53-3759-ea8d-dd93ca87c938>
2012-11-17 14:48:50 +0000

    优化分派策略，运行期复杂度降为O(log N)。(习题11-8)

--- finite_state_machine/event_dispatcher.h
+++ finite_state_machine/event_dispatcher.h
@@ -1,11 +1,12 @@
 #ifndef __FSM_event_dispatcher_H__
 #define __FSM_event_dispatcher_H__
 
-template <class _row, class _next>
+template <class _row, class _left, class _right>
 struct event_dispatcher
 {
 	typedef typename _row::fsm_t fsm_t;
 	typedef typename _row::event event;
+	typedef event_dispatcher type;
 
 	static int dispatch(fsm_t &fsm, int state, const event &e)
 	{
@@ -14,15 +15,21 @@ struct event_dispatcher
 			_row::execute(fsm, e);
 			return _row::next_state;
 		}
+		else if (state < _row::current_state)
+		{
+			return _left::type::dispatch(fsm, state, e);
+		}
 		else
 		{
-			return _next::dispatch(fsm, state, e);
+			return _right::type::dispatch(fsm, state, e);
 		}
 	}
 };
 
 struct default_event_dispatch
 {
+	typedef default_event_dispatch type;
+
 	template <class _fsm, class _event>
 	static int dispatch(finite_state_machine<_fsm> &fsm, int state, const _event &e)
 	{
